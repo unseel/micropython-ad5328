3d10f992fa5c746b373f6168b4296e5912604edb
robbietree8 <wangzg1988@gmail.com>
2022-04-11 18:13:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# AD5328(WIP)
+
+## Description
+
+`AD5328`是一个8通道、12位精度的数模转换器，兼容`SPI`, `QSPI`, `Microwave`和`DSP`接口。
+
+## Features
+
+1. 给指定通道施加指定的电压
+
+## Example
+
+```python
+from machine import SPI, Pin
+import ad5328
+
+# define spi/cs, esp32-s for example
+spi = SPI(2, 10000)
+cs = machine.Pin(5, Pin.OUT)
+
+# init dac8568
+ad5328_ = ad5328.AD5328(spi, cs)
+
+# set voltage
+ad5328_.set_voltage(ad5328.Channel.A, 4095)
+```
+
+## Reference
+
+- [data sheet](https://www.analog.com/media/en/technical-documentation/data-sheets/ad5308_5318_5328.pdf)
+
---
+++ ad5328.py
@@ -0,0 +1,109 @@
+from micropython import const
+
+class Channel:
+  """Channel Enum."""
+
+  A = const(0x00)
+  B = const(0x01)
+  C = const(0x02)
+  D = const(0x03)
+  E = const(0x04)
+  F = const(0x05)
+  G = const(0x06)
+  H = const(0x07)
+
+class Mode:
+  """Mode Enum."""
+  GAIN = const(0x00)
+  LDAC = const(0x01)
+  POWER_DOWN = const(0x02)
+  RESET = const(0x03)
+
+class Prefix:
+  """Control Enum."""
+  DacWrite = const(0x00)
+  Control = const(0x01)
+
+class PresetData:
+  """Preset Enum."""
+  DoubleGain = const(0x030) # 0000,0011,0000
+  LDACLow = const(0x000) # 0000,0000,0000
+  LDACHigh = const(0x001) # 0000,0000,0001
+  LDACSingle = const(0x002) # 0000,0000,0010
+
+class Payload:
+  """Data Input Register Format"""
+
+  def __init__(self):
+    self.prefix = const(0x00) # 1bit
+    self.address = const(0x00) # 3bits
+    self.data = const(0x00) # 12bits
+  
+  def set_prefix(self, prefix):
+    self.prefix = prefix
+  
+  def set_address(self, address):
+    self.address = address
+
+  def set_data(self, data):
+    self.data = data
+  
+  def to_bytes(self):
+    result = 0
+    result = result | self.prefix << 15
+    result = result | self.address << 12
+    result = result | self.data << 0
+    return result.to_bytes(2, 'big')
+
+class AD5328:
+  """Implementation for ad5328"""
+
+  def __init__(self, spi, cs):
+    self.spi = spi
+    self.cs = cs
+  
+  # def use_internal_reference(self):
+  #   payload:Payload = Payload()
+  #   payload.set_control(Prefix.InternalReferenceStatic)
+  #   payload.set_feature(Feature.InternalReference)
+  #   self.__write__(payload)
+
+  def set_gain(self):
+    payload:Payload = Payload()
+    payload.set_prefix(Prefix.Control)
+    payload.set_address(Mode.GAIN)
+    payload.set_data(PresetData.DoubleGain)
+    self.__write__(payload)
+
+  """set voltage to specified channel"""
+  def set_voltage(self, channel, voltage):
+    payload:Payload = self.set_voltage0(channel, voltage)
+    payload.set_prefix(Prefix.DacWrite)
+    self.__write__(payload)
+
+  def set_voltage0(self, channel, voltage):
+    voltage = self.check_voltage(voltage)
+    payload:Payload = Payload()
+    payload.set_address(channel)
+    payload.set_data(voltage)
+    return payload
+  
+  def check_voltage(self, voltage):
+    if voltage < 0:
+      voltage = 0
+    if voltage > 4095:
+      voltage = 4095
+    return voltage
+
+  def __write__(self, payload):
+    self.cs.off()
+    result = self.spi.write(payload.to_bytes())
+    print(result)
+    self.cs.on()
+
+if __name__ == '__main__':
+  payload = Payload()
+  payload.set_prefix(Prefix.DacWrite)
+  payload.set_address(Channel.A)
+  payload.set_data(4095)
+  print(payload.to_bytes())
